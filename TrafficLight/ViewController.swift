327e62863e30e542d6cdd102017e61ec413f33c3
AndriiSilvestrov <andrew.silvestrov@gmail.com>
2020-11-18 17:17:29 +0200

    2. The algorithm of actions is prescribed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColor.alpha = 0.3
+        redColor.layer.cornerRadius = 60
+        yellowColor.alpha = 0.3
+        yellowColor.layer.cornerRadius = 60
+        greenColor.alpha = 0.3
+        greenColor.layer.cornerRadius = 60
+        actionButton.layer.cornerRadius = 60
+        actionButton.layer.borderWidth = 1.5
+        actionButton.layer.borderColor = UIColor.systemBlue.cgColor
+        actionButton.clipsToBounds = true
+        
     }
 
+    @IBAction func actionButtomPressed() {
+        actionButton.setTitle("NEXT", for: .normal)
+        if redColor.alpha < 1 && yellowColor.alpha < 1 {
+            redColor.alpha = 1
+            greenColor.alpha = 0.3
+        } else if yellowColor.alpha < 1 && greenColor.alpha < 1 {
+            yellowColor.alpha = 1
+            redColor.alpha = 0.3
+        } else if greenColor.alpha < 1 && redColor.alpha < 1 {
+            greenColor.alpha = 1
+            yellowColor.alpha = 0.3
+        }
 
+    }
+    
 }
 
