a9115191088300e06836e7228c923b7cddb22478
Andrii Silvestrov <74545742+silvestrovv@users.noreply.github.com>
2020-11-24 13:28:46 +0200

    2.2 Fixed interaction algorithm

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,17 +8,67 @@
 
 import UIKit
 
+enum TrafficLightColors {
+    case red
+    case yellow
+    case green
+    case none
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
     
+    @IBOutlet var colorChangeButton: UIButton!
+    
+    private var currentColor = TrafficLightColors.none
+    
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redColor.layer.cornerRadius = redColor.frame.width / 2
+        redColor.alpha = lightOff
         
+        yellowColor.layer.cornerRadius = yellowColor.frame.width / 2
+        yellowColor.alpha = lightOff
+        
+        greenColor.layer.cornerRadius = greenColor.frame.width / 2
+        greenColor.alpha = lightOff
+        
+        colorChangeButton.layer.cornerRadius = colorChangeButton.frame.width / 4
+        colorChangeButton.layer.borderWidth = 1.2
+        colorChangeButton.layer.borderColor = UIColor.systemBlue.cgColor
+        colorChangeButton.clipsToBounds = true
         
     }
 
-
+    @IBAction func trafficLightColorChange() {
+        colorChangeButton.setTitle("NEXT", for: .normal)
+        
+        switch currentColor {
+        case .red:
+            greenColor.alpha = lightOff
+            redColor.alpha = lightOn
+            currentColor = .yellow
+        case .yellow:
+            redColor.alpha = lightOff
+            yellowColor.alpha = lightOn
+            currentColor = .green
+        case .green:
+            yellowColor.alpha = lightOff
+            greenColor.alpha = lightOn
+            currentColor = .red
+        default:
+            redColor.alpha = lightOn
+            currentColor = .yellow
+        }
+        
+    }
+    
 }
 
